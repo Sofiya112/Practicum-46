9eda46b952a530629d4a546ca93352bcfc196cbb
Sofiya112 <sofiya032005@gmail.com>
2025-06-05 11:20:36 +0300

    Add files via upload

---
+++ Task 2 pr 46/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_2_pr_46
+{
+    class Program
+    {
+        enum Days
+        {
+            SAT = 0,
+            SUN = 1,
+            MON = 2,
+            TUE = 3,
+            WED = 4,
+            THU = 5,
+            FRI = 6
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (Days day in Enum.GetValues(typeof(Days)))
+            {
+                Console.WriteLine($"{day} - имеет значение {(int)day}");
+            }
+
+            Console.Read();
+        }
+    }
+}
