a66c31f16502e36f3ba7fac18da9355bd967c88a
Kab-35-16 <Kab-35-16@KAB-31-3->
2025-06-05 11:19:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_2_pr_46
+{
+    class Program
+    {
+        enum Months
+        {
+            Январь,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (int i in Enum.GetValues(typeof(Months)))
+            {
+                string monthName = Enum.GetName(typeof(Months), i);
+                Console.WriteLine($"{i} - {monthName}");
+            }
+
+            Console.Read();
+        }
+    }
+}
